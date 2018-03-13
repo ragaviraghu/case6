c6d22c536a8c6ca9c986943a48aad3d33bd0c97d
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-13 07:23:43 -0700

    Create sumofdigits.cpp

---
+++ sumofdigits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{int num,r,sum=0;
+cout<<"Enter the number : "<<endl;
+cin>>num;
+while(num!=0)
+{r=num%10;
+sum=sum+r;
+num=num/10;
+}
+cout<<"The sum of digits of the diven number is : "<<sum<<endl;
+return 0;
+}
