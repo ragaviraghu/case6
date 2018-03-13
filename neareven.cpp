f8eded1bef0054e823951572d3700fbfa54d2e3f
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-13 07:31:19 -0700

    Create neareven.cpp

---
+++ neareven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{int num;
+cout<<"Enter the number : "<<endl;
+cin>>num;
+if(num%2==0)
+{cout<<"The neraest even number is : "<<num<<endl;
+}
+else
+{cout<<"The neraest even number is : "<<num-1<<endl;
+}
+return 0;
+}
