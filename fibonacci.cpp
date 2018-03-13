86edd10dc2bf535a22672f312f60e60d4b7cfef5
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-13 09:54:39 -0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-int n,f1=0,f2=1,f3;
+int num,f1=0,f2=1,f3;
 cout<<"Enter the number : "<<endl;
-cin>>n;
-cout<<"The fibonacci series for "<<n<<" terms are : "<<endl;
-for(int i=1;i<=n;++i)
+cin>>num;
+cout<<"The fibonacci series for "<<num<<" terms are : "<<endl;
+for(int i=1;i<=num;++i)
 {
 cout<<f2<<endl;	
 f3=f1+f2;
