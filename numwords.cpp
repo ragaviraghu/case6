48245157820f347c432ded331f05f484b0f31b00
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-13 07:17:48 -0700

    Create numwords.cpp

---
+++ numwords.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int n;
+cout<<"Enter the number : "<<endl;
+cin>>n;
+switch(n)
+{case 1:cout<<"One"<<endl;
+break;
+case 2:cout<<"Two"<<endl;
+break;
+case 3:cout<<"Three"<<endl;
+break;
+case 4:cout<<"Four"<<endl;
+break;
+case 5:cout<<"Five"<<endl;
+break;
+case 6:cout<<"Six"<<endl;
+break;
+case 7:cout<<"Seven"<<endl;
+break;
+case 8:cout<<"Eight"<<endl;
+break;
+case 9:cout<<"Nine"<<endl;
+break;
+case 10:cout<<"Ten"<<endl;
+break;
+default:cout<<"Invalid Input"<<endl;
+break;
+}
+return 0;
+}
